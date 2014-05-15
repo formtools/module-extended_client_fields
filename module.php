ffe85f7786de7f2809859ed7c47ac537869f4a85
Ben Keen <ben.keen@gmail.com>
2014-05-14 21:19:17 -0700

    1.2.3

--- module.php
+++ module.php
@@ -10,8 +10,8 @@
 $MODULE["author"]          = "Encore Web Studios";
 $MODULE["author_email"]    = "formtools@encorewebstudios.com";
 $MODULE["author_link"]     = "http://www.encorewebstudios.com";
-$MODULE["version"]         = "1.2.2";
-$MODULE["date"]            = "2011-10-19";
+$MODULE["version"]         = "1.2.3";
+$MODULE["date"]            = "2011-11-05";
 $MODULE["origin_language"] = "en_us";
 
 // define the module navigation - the keys are keys defined in the language file. This lets
