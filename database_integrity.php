7ca944dd92799fc5f273d29e4b1979c3acfcafde
Ben Keen <ben.keen@gmail.com>
2014-05-14 21:18:35 -0700

    1.2.2

--- database_integrity.php
+++ database_integrity.php
@@ -97,3 +97,112 @@ $STRUCTURE["tables"]["module_extended_client_field_options"] = array(
     "Default" => ""
   )
 );
+
+$HOOKS = array();
+$HOOKS["1.2.2"] = array(
+  array(
+    "hook_type"       => "template",
+    "action_location" => "admin_edit_client_main_top",
+    "function_name"   => "",
+    "hook_function"   => "ecf_display_fields",
+    "priority"        => "50"
+  ),
+  array(
+    "hook_type"       => "template",
+    "action_location" => "admin_edit_client_main_middle",
+    "function_name"   => "",
+    "hook_function"   => "ecf_display_fields",
+    "priority"        => "50"
+  ),
+  array(
+    "hook_type"       => "template",
+    "action_location" => "admin_edit_client_main_bottom",
+    "function_name"   => "",
+    "hook_function"   => "ecf_display_fields",
+    "priority"        => "50"
+  ),
+  array(
+    "hook_type"       => "template",
+    "action_location" => "admin_edit_client_settings_top",
+    "function_name"   => "",
+    "hook_function"   => "ecf_display_fields",
+    "priority"        => "50"
+  ),
+  array(
+    "hook_type"       => "template",
+    "action_location" => "admin_edit_client_settings_bottom",
+    "function_name"   => "",
+    "hook_function"   => "ecf_display_fields",
+    "priority"        => "50"
+  ),
+  array(
+    "hook_type"       => "template",
+    "action_location" => "admin_edit_view_client_map_filter_dropdown",
+    "function_name"   => "",
+    "hook_function"   => "ecf_display_extended_field_options",
+    "priority"        => "50"
+  ),
+  array(
+    "hook_type"       => "template",
+    "action_location" => "head_bottom",
+    "function_name"   => "",
+    "hook_function"   => "ecf_insert_head_js",
+    "priority"        => "50"
+  ),
+  array(
+    "hook_type"       => "code",
+    "action_location" => "end",
+    "function_name"   => "ft_admin_update_client",
+    "hook_function"   => "ecf_admin_save_extended_client_fields",
+    "priority"        => "50"
+  ),
+  array(
+    "hook_type"       => "template",
+    "action_location" => "edit_client_main_top",
+    "function_name"   => "",
+    "hook_function"   => "ecf_display_fields",
+    "priority"        => "50"
+  ),
+  array(
+    "hook_type"       => "template",
+    "action_location" => "edit_client_main_middle",
+    "function_name"   => "",
+    "hook_function"   => "ecf_display_fields",
+    "priority"        => "50"
+  ),
+  array(
+    "hook_type"       => "template",
+    "action_location" => "edit_client_main_bottom",
+    "function_name"   => "",
+    "hook_function"   => "ecf_display_fields",
+    "priority"        => "50"
+  ),
+  array(
+    "hook_type"       => "template",
+    "action_location" => "edit_client_settings_top",
+    "function_name"   => "",
+    "hook_function"   => "ecf_display_fields",
+    "priority"        => "50"
+  ),
+  array(
+    "hook_type"       => "template",
+    "action_location" => "edit_client_settings_bottom",
+    "function_name"   => "",
+    "hook_function"   => "ecf_display_fields",
+    "priority"        => "50"
+  ),
+  array(
+    "hook_type"       => "code",
+    "action_location" => "end",
+    "function_name"   => "ft_update_client",
+    "hook_function"   => "ecf_client_save_extended_client_fields",
+    "priority"        => "50"
+  ),
+  array(
+    "hook_type"       => "code",
+    "action_location" => "start",
+    "function_name"   => "ft_get_view_filter_sql",
+    "hook_function"   => "ecf_update_view_filter_sql_placeholders",
+    "priority"        => "50"
+  )
+);
